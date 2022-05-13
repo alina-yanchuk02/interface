c808abb4eb9882fbc922edaa52335baf9f2e4a8d
alina-yanchuk02 <alina-yanchuk02@outlook.pt>
2022-05-13 22:02:45 +0100

    Updates

---
+++ precache-manifest.10c6f46045d47521f3b64fd54af5da81.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbce8ed560f7bf833160",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/0.78c87313.chunk.js"
+  },
+  {
+    "revision": "c9a2cb2fbaf899ba3339",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/1.e8fede00.chunk.js"
+  },
+  {
+    "revision": "1707b09a30ebab8056e1",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/2.056b9951.chunk.js"
+  },
+  {
+    "revision": "5d648f9f85cc3a98f61b",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/3.f40dc17e.chunk.js"
+  },
+  {
+    "revision": "ee997ce115ec76a823dd",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/css/main.76231fd4.chunk.css"
+  },
+  {
+    "revision": "ee997ce115ec76a823dd",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/main.1d01b8cc.chunk.js"
+  },
+  {
+    "revision": "bc31e96f55276161de59",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/runtime~main.81c8897b.js"
+  },
+  {
+    "revision": "07afe4bbe1048447c4af",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/6.f3402788.chunk.js"
+  },
+  {
+    "revision": "8c5f2dde5641433f345e",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/7.1ef85923.chunk.js"
+  },
+  {
+    "revision": "77628dfe9846d2b6d5b9",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/css/8.30c36dcb.chunk.css"
+  },
+  {
+    "revision": "77628dfe9846d2b6d5b9",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/8.99d55fe1.chunk.js"
+  },
+  {
+    "revision": "6bda99950511a208f91e",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/css/9.f10d07ce.chunk.css"
+  },
+  {
+    "revision": "6bda99950511a208f91e",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/9.679069c3.chunk.js"
+  },
+  {
+    "revision": "0f07407e72956d8c2e72",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/css/10.5c6ab0f9.chunk.css"
+  },
+  {
+    "revision": "0f07407e72956d8c2e72",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/10.57a08442.chunk.js"
+  },
+  {
+    "revision": "795b2902ee4d9cdf8651",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/css/11.7cdc4da4.chunk.css"
+  },
+  {
+    "revision": "795b2902ee4d9cdf8651",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/11.30428743.chunk.js"
+  },
+  {
+    "revision": "af69dedeba9a5a8b8c58",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/12.f8fce961.chunk.js"
+  },
+  {
+    "revision": "9b0ed0f023726b0e0cb2",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/css/13.e559e098.chunk.css"
+  },
+  {
+    "revision": "9b0ed0f023726b0e0cb2",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/13.631415fd.chunk.js"
+  },
+  {
+    "revision": "ffab7f6556c2b11bacb5",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/14.4642beaa.chunk.js"
+  },
+  {
+    "revision": "f696e80b0c325a15c778",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/css/15.89c8b8e5.chunk.css"
+  },
+  {
+    "revision": "f696e80b0c325a15c778",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/15.763722fb.chunk.js"
+  },
+  {
+    "revision": "05d38085d34b430b00c7",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/js/16.03480c66.chunk.js"
+  },
+  {
+    "revision": "e03f561f432356fc56989cc13cffdba4",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/nav.e03f561f.jpg"
+  },
+  {
+    "revision": "2648cf6a1b5d85ff69262e5193f821b5",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/aveiro.2648cf6a.png"
+  },
+  {
+    "revision": "8fe00f4f37ff630d8367380f0349fbfe",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/fccn.8fe00f4f.png"
+  },
+  {
+    "revision": "c863f8028c2505f92540e0ba7c379002",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/PermanentMarker-Regular.c863f802.ttf"
+  },
+  {
+    "revision": "51218abdbefa3dc8b164f07791d44e86",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/card1.51218abd.png"
+  },
+  {
+    "revision": "7aad48eb28ad8a7755124ea94fc53ee7",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/card2.7aad48eb.png"
+  },
+  {
+    "revision": "dc3377d23f3af36045167ccd5c36e0c3",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/alinaa.dc3377d2.png"
+  },
+  {
+    "revision": "bb5593d17807e8eff7aec1cda9025470",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/alinatrifan.bb5593d1.png"
+  },
+  {
+    "revision": "57d054a230e222cc7feabf198d79d27f",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/jlo.57d054a2.png"
+  },
+  {
+    "revision": "11280a8667a12fb71a7e35d83820afa4",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/olga.11280a86.png"
+  },
+  {
+    "revision": "69deec04e64cde29d4484539a510455c",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/database.69deec04.jpg"
+  },
+  {
+    "revision": "fa238e3122864417975086535d003037",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/1in5.fa238e31.png"
+  },
+  {
+    "revision": "25b4adab2909cc9dbb4cadb36728e818",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/observador.25b4adab.png"
+  },
+  {
+    "revision": "ecc64da89cf5b2c347ee1c125f347096",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/dn.ecc64da8.png"
+  },
+  {
+    "revision": "27388ee1968729ff31f8359393e5e022",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/jn.27388ee1.png"
+  },
+  {
+    "revision": "ccd8b2c554286386ec01b7e8fb4286c6",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/visao.ccd8b2c5.png"
+  },
+  {
+    "revision": "a65d32543d6810e74539a126d5f16a2b",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/expresso.a65d3254.png"
+  },
+  {
+    "revision": "ea26a0640cbb95d42b57e36b18f7fda1",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/cm.ea26a064.png"
+  },
+  {
+    "revision": "74bbcb727945ae901c63bce921005d2e",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/sabado.74bbcb72.png"
+  },
+  {
+    "revision": "2b085a99cfae061650d605562b55a34b",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/bola.2b085a99.png"
+  },
+  {
+    "revision": "f354ef939e848268b0df8960a2f6375d",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/publico.f354ef93.png"
+  },
+  {
+    "revision": "c22dd718a5a281db799c43ed183ed051",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/lr.c22dd718.jpg"
+  },
+  {
+    "revision": "ee10b67428df0fd3dca3426b376d8d8a",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/svm.ee10b674.png"
+  },
+  {
+    "revision": "4ea3b72f5e629ec7603a53dde16a9468",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/nb.4ea3b72f.png"
+  },
+  {
+    "revision": "f0e86ec0eef88f7048667df95d210186",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/lstm.f0e86ec0.jpeg"
+  },
+  {
+    "revision": "309125855e63329e4d836006cf7e27ab",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/bilstm.30912585.png"
+  },
+  {
+    "revision": "38189f9b873c4bfb82bc4d0ccb3481dd",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/bert.38189f9b.png"
+  },
+  {
+    "revision": "8d7f950ebda4bb76fd68391a1188bdce",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/random.8d7f950e.png"
+  },
+  {
+    "revision": "ca799199a5141369a50154ac7408cc5b",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/arch.ca799199.png"
+  },
+  {
+    "revision": "b7eb53aafa471993c11bf60dba0a0e4d",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/pin.b7eb53aa.png"
+  },
+  {
+    "revision": "8427fbf99603f618b13a87324592e973",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/news0.8427fbf9.png"
+  },
+  {
+    "revision": "cd363249ba914979b44762463e3dff1b",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/news1.cd363249.png"
+  },
+  {
+    "revision": "1e53fbfd26230103d1d40da68b639c63",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/news2.1e53fbfd.png"
+  },
+  {
+    "revision": "f9f4a351bba759caa38c2fab3249f58b",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/news3.f9f4a351.png"
+  },
+  {
+    "revision": "d768ab2a766bec5f275e6fd4167037a4",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/news4.d768ab2a.png"
+  },
+  {
+    "revision": "937e346cd783bc44c5b055d6672ebf1b",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/news5.937e346c.png"
+  },
+  {
+    "revision": "d914b8db98e921ee297f5c981f2aa913",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/logo_1.d914b8db.png"
+  },
+  {
+    "revision": "830cfc6b8c5468aff2d93412ede41445",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/logo_2.830cfc6b.png"
+  },
+  {
+    "revision": "ad0609e1b3f6548f4aa909a492ae899d",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/logo_3.ad0609e1.png"
+  },
+  {
+    "revision": "a144a2fa7737301ce57ef31d33910f88",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/topic0.a144a2fa.png"
+  },
+  {
+    "revision": "598c68938cf7a5f0c0c659f417c3fbd3",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/topic1.598c6893.png"
+  },
+  {
+    "revision": "023f671eae10908419f4f38e5645dc4c",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/topic2.023f671e.png"
+  },
+  {
+    "revision": "ecef7c501b97abdf2607b213f915c9b8",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/topic3.ecef7c50.png"
+  },
+  {
+    "revision": "8a42986364f861c3c578be1334af7c01",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/topic4.8a429863.png"
+  },
+  {
+    "revision": "e1b947f73415f093f1bde3902146aa14",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/topic5.e1b947f7.png"
+  },
+  {
+    "revision": "771b5813fe416d782c1b3dfe89059b4c",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/topic6.771b5813.png"
+  },
+  {
+    "revision": "d479fd1d1a405e67b3d63f6a9b0679a9",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/topic7.d479fd1d.png"
+  },
+  {
+    "revision": "b2e8526edf04a6066f2ddc783390c6fc",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/topic8.b2e8526e.png"
+  },
+  {
+    "revision": "f1c586a9a15fc254cee0d4e02864adec",
+    "url": "https://alina-yanchuk02.github.io/estigma/static/media/topic9.f1c586a9.png"
+  },
+  {
+    "revision": "c6f00083b05f3923f6946e7a5d14f2d6",
+    "url": "https://alina-yanchuk02.github.io/estigma/index.html"
+  }
+];
